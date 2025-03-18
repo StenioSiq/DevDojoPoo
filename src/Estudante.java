2e3d13f750c0c05a02d37ef0d14434eb8a0e6d5d
steniosqr <steniosqr@gmail.com>
2025-03-17 21:01:14 -0300

    Arquivos iniciais de estudo

---
+++ src/Estudante.java
@@ -0,0 +1,7 @@
+public class Estudante {
+
+    public String nome;
+    public int idade;
+    public String sexo;
+
+}
---
+++ src/EstudanteTeste.java
@@ -0,0 +1,10 @@
+public class EstudanteTeste {
+
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Pedro";
+        estudante.idade = 20;
+        estudante.sexo = "M";
+    }
+
+}
